14fac45cd2a607736968150416e45e0507397372
strive <2205487298@qq.com>
2025-02-20 14:05:54 +0800

    update

---
+++ src/main/java/com/strive/ThreadPractice/first_practice/MyThread.java
@@ -0,0 +1,26 @@
+package com.strive.ThreadPractice.first_practice;
+
+public class MyThread extends Thread {
+
+    static int ticket = 0;
+
+    @Override
+    public void run() {
+        //
+        while (true) {
+            synchronized (MyThread.class) {
+                if(ticket == 1000){
+                    break;
+                }else{
+                    try {
+                        Thread.sleep(1500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    ticket++;
+                    System.out.println(Thread.currentThread().getName() +"正在卖第" + ticket + "张票，还剩" + (1000 - ticket) + "张票");
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/strive/ThreadPractice/first_practice/TestThread.java
@@ -0,0 +1,19 @@
+package com.strive.ThreadPractice.first_practice;
+
+public class TestThread {
+    public static void main(String[] args) {
+
+        //一共有1000张电影票，可以在两个窗口领取，每次领取时间为3000ms
+        //使用多线程模拟买票过程并打印剩余电影票数量
+
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+
+        t1.setName("窗口1");
+        t2.setName("窗口2");
+
+        t1.start();
+        t2.start();
+
+    }
+}
